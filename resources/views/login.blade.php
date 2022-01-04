38fadb922de1dcd780c284256998be11c5327880
Zynn02 <robertio2202001@gmail.com>
2022-01-04 23:42:49 +0700

    master/login/layoutcss/logo
    kurang sempurna

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+@extends('Master.layout')
+
+@section('body')
+    <div class="d-flex justify-content-center ">
+        <form action="/login">      
+            <h1> Welcome Back Cunt</h1>
+        <input type="email" name="email" placeholder="email">
+        <br>
+        <input type="password" name="password" placeholder="password">
+        <br>
+        <input type="checkbox" name="remember" id=""> Remember Me
+        <br>
+        <input type="submit" value="login">
+        </form>      
+    </div>
+@endsection
