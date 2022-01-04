bec497d9041f452d9c582ec75ce3c5c689995135
TYR <natdavin@gmail.com>
2022-01-05 00:08:11 +0700

    Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    //
+    public function productVerification(Request $req){
+        $rules = [
+            'productName' => 'required|unique:products,name|min:5',
+            'description' => 'required|min:5',
+            'price' => 'required|numeric|gt:0',
+            'category' => 'required',
+            'image' => 'required|mimes:jpg'
+        ];
+
+        $validation = Validator::make($req->all(), $rules);
+
+        if($validation->fails()){
+            return back()->withErrors([$validation], 'insert');
+        }
+    }
 }
